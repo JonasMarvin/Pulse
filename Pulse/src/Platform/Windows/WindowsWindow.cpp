b298b8ffbb99dd71bd29277e4b3437e4de0a00f9
Jonas-Marvin Lühr <95620676+JonasMarvin@users.noreply.github.com>
2023-09-09 18:50:31 +0200

    Added several events for GLFW

--- Pulse/src/Platform/Windows/WindowsWindow.cpp
+++ Pulse/src/Platform/Windows/WindowsWindow.cpp
@@ -2,10 +2,18 @@
 
 #include "WindowsWindow.h"
 #include "Pulse/Log.h"
+#include "Pulse/Events/Events.h"
 
 namespace Pulse {
 
 	static bool isGLFWInitialized = false;
+
+	std::shared_ptr<Pulse::Events::Event<int, int>> Pulse::Events::Window::WindowResizeEvent = Pulse::Events::Event<int, int>::Create();
+	std::shared_ptr<Pulse::Events::Event<>> Pulse::Events::Window::WindowCloseEvent = Pulse::Events::Event<>::Create();
+	std::shared_ptr<Pulse::Events::Event<int, int, int, int>> Pulse::Events::Input::KeyEvent = Pulse::Events::Event<int, int, int, int>::Create();
+	std::shared_ptr<Pulse::Events::Event<int, int, int>> Pulse::Events::Input::MouseEvent = Pulse::Events::Event<int, int, int>::Create();
+	std::shared_ptr<Pulse::Events::Event<double, double>> Pulse::Events::Input::ScrollEvent = Pulse::Events::Event<double, double>::Create();
+	std::shared_ptr<Pulse::Events::Event<double, double>> Pulse::Events::Input::MouseMovedEvent = Pulse::Events::Event<double, double>::Create();
 
 	Window* Window::Create(const WindowProperties& properties) {
 		return new WindowsWindow(properties);
@@ -21,6 +29,9 @@ namespace Pulse {
 		if (!isGLFWInitialized) {
 			int success = glfwInit();
 			PLS_CORE_ASSERT(success, "Could not initialize GLFW!");
+			glfwSetErrorCallback([](int error, const char* description) {
+				PLS_CORE_ERROR("GLFW Error ({0}): {1}", error, description);
+			});
 			isGLFWInitialized = true;
 		}
 
@@ -28,6 +39,34 @@ namespace Pulse {
 		glfwMakeContextCurrent(window_);
 		glfwSetWindowUserPointer(window_, &data_);
 		SetVSync(true);
+
+		// Set GLFW callbacks
+		glfwSetWindowSizeCallback(window_, [](GLFWwindow* window, int width, int height) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+			data.height = height;
+			data.width = width;
+			Pulse::Events::Window::WindowResizeEvent->Trigger(width, height);
+		});
+
+		glfwSetWindowCloseCallback(window_, [](GLFWwindow* window) {
+			Pulse::Events::Window::WindowCloseEvent->Trigger();
+		});
+
+		glfwSetKeyCallback(window_, [](GLFWwindow* window, int key, int scancode, int action, int mods) {
+			Pulse::Events::Input::KeyEvent->Trigger(key, scancode, action, mods);
+		});
+
+		glfwSetMouseButtonCallback(window_, [](GLFWwindow* window, int button, int action, int mods) {
+			Pulse::Events::Input::MouseEvent->Trigger(button, action, mods);
+		});
+
+		glfwSetScrollCallback(window_, [](GLFWwindow* window, double xOffset, double yOffset) {
+			Pulse::Events::Input::ScrollEvent->Trigger(xOffset, yOffset);
+		});
+
+		glfwSetCursorPosCallback(window_, [](GLFWwindow* window, double xPos, double yPos) {
+			Pulse::Events::Input::MouseMovedEvent->Trigger(xPos, yPos);
+		});
 	}
 
 	WindowsWindow::~WindowsWindow() {
