ba1a7423abda65b71d77c90991513d2a8d03e947
Jonas-Marvin Lühr <95620676+JonasMarvin@users.noreply.github.com>
2023-09-19 19:08:07 +0200

    Printing OpenGL info to screen

--- Pulse/src/Pulse/Modules/Rendering/OpenGL/OpenGLContext.cpp
+++ Pulse/src/Pulse/Modules/Rendering/OpenGL/OpenGLContext.cpp
@@ -14,7 +14,8 @@ namespace Pulse::Modules::Rendering {
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		PLS_CORE_ASSERT(status, "Failed to initialize Glad!");
 
-		PLS_CORE_INFO("OpenGL Context initialized with GLFWwindow");
+		PLS_CORE_INFO("OpenGL Context initialized with GLFWwindow.");
+		PLS_CORE_INFO("OpenGL info: Vendor: {0} | Renderer: {1} | Version {2}", (const char*)glGetString(GL_VENDOR), (const char*)glGetString(GL_RENDERER), (const char*)glGetString(GL_VERSION));
 	}
 
 	void OpenGLContext::SwapBuffers() {
